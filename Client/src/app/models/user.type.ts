f6327d8890e5efeb6448f89f7e5ad7227424bc25
ljw <jiawei.li515@gmail.com>
2025-02-17 11:31:20 +0100

    add user type

---
+++ Client/src/app/models/user.type.ts
@@ -0,0 +1,12 @@
+export type User = {
+    email: string;
+    password: boolean;
+  };
+
+export type Address = {
+    firstName: string;
+    lastName: string;
+    street: string;
+    city: string;
+    zipcode: string;
+}
