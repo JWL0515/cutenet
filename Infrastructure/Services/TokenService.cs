ecbee70e0579081d9ac3aa7e680a7a8fb95bf430
ljw <jiawei.li515@gmail.com>
2025-02-17 16:21:17 +0100

    add tokenservice

---
+++ Infrastructure/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Services
+{
+    class TokenService
+    {  
+        public string GenerateToken(Guid userId, string email)
+        {
+            // 1.step: prepare tokenHandler, credential, claims
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "replaceThisLaterForMoreSecurity"u8.ToArray();
+            var securityKey = new SymmetricSecurityKey(key);
+            var credential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // help for validation (like backlist)
+                new(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new(JwtRegisteredClaimNames.Email, email),
+            };
+
+            // 2.step: prepare tokenDescriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(240),
+                Issuer = "JWL",
+                SigningCredentials = credential
+            };
+
+            // 3.step: create token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Dtos;
+
+namespace WebApi.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
