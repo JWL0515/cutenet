aca8cfb72b1984aae522f59100e961db97908b44
ljw <jiawei.li515@gmail.com>
2025-02-18 18:20:05 +0100

    delet .net8 for .net 9

--- WebApi/Dtos/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Dtos
-{
-    public class RegisterDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- WebApi/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.Dtos
-{
-    public class UserDto
-    {
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Token { get; set; }
-    }
-}
