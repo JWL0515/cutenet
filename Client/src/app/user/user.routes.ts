62d9c3d045984f74f60ca9062fc63c0a724b6654
ljw <jiawei.li515@gmail.com>
2025-02-25 14:47:49 +0100

    delete address

--- Client/src/app/user/user.routes.ts
+++ Client/src/app/user/user.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
 import { UserComponent } from './user.component';
 import { authGuard } from '../guard/auth.guard';
-import { AddressComponent } from '../components/address/address.component';
 
 export const routes: Route[] = [
     // if logged then shows Address on ../user
@@ -21,8 +20,5 @@ export const routes: Route[] = [
         path: 'register',
         component: RegisterComponent,
     },
-    {
-        path: 'address',
-        component: AddressComponent,
-    },
+  
   ];
