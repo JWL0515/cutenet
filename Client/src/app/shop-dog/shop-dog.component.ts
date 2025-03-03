e60087b93097078806b128c88b27dbea81d7fc4d
ljw <jiawei.li515@gmail.com>
2025-03-03 17:12:08 +0100

    add types for dog shop

--- Client/src/app/shop-dog/shop-dog.component.ts
+++ Client/src/app/shop-dog/shop-dog.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DogProduct } from '../models/dogProduct.type';
+import { DogBrand } from '../models/dogBrand.type';
+import { DogCategory } from '../models/dogCategory.type';
 
 @Component({
   selector: 'app-shop-dog',
@@ -7,5 +10,7 @@ import { Component } from '@angular/core';
   styleUrl: './shop-dog.component.scss'
 })
 export class ShopDogComponent {
-
+  products: DogProduct[] = [];
+  brands: DogBrand[] = [];
+  categories: DogCategory[] = [];
 }
