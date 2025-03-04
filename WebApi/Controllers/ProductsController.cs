76aa8a12ef992035d8d603e8aaec0a2eb42a45dc
ljw <jiawei.li515@gmail.com>
2025-03-04 11:02:57 +0100

    add GetAllProducts

--- WebApi/Controllers/ProductsController.cs
+++ WebApi/Controllers/ProductsController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Entities;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(DogProductDbContext context) : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
+        {
+            IQueryable<Product> products = context.Products;
+
+
+            return Ok(await products.Include(p => p.Brand).ToListAsync());
+        }
     }
 }
