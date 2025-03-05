0ef44fcb2755369aa9842fb165903707d1575ebe
ljw <jiawei.li515@gmail.com>
2025-03-05 08:21:27 +0100

    add params for GET

--- Client/src/app/shop-dog/shop-dog.component.ts
+++ Client/src/app/shop-dog/shop-dog.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DogProduct } from '../models/dogProduct.type';
 import { DogBrand } from '../models/dogBrand.type';
 import { DogCategory } from '../models/dogCategory.type';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { QueryParameter } from '../models/queryParameter';
+import { response } from 'express';
 
 @Component({
   selector: 'app-shop-dog',
@@ -13,4 +16,33 @@ export class ShopDogComponent {
   products: DogProduct[] = [];
   brands: DogBrand[] = [];
   categories: DogCategory[] = [];
+  sortOptions = [
+    {name: 'Price: low to high', value: 'priceAsc'},
+    {name: 'Price: high to low', value: 'priceDesc'}
+  ]
+  queryparams = new QueryParameter();
+
+  params = new HttpParams();
+  
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  http = inject(HttpClient);
+
+  getProducts() {
+    this.params = this.params.append('page', 2);
+    this.params = this.params.append('pageSize', this.queryparams.pageSize); 
+    this.params = this.params.append('sort', this.queryparams.sortBy); 
+    return this.http.get<DogProduct[]>('https://localhost:7284/api/Products', {params:this.params}).subscribe(
+      (response) => { 
+        console.log('response', response);
+        this.products = response;
+      });
+    // return this.http.get<DogProduct[]>('https://localhost:7284/api/Products', {params:this.params}).subscribe(
+    // (response) => { 
+    //   console.log('response', response);
+    //   this.products = response;
+    // });
+  }
 }
