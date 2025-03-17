efa36d50bf5120e094e2ba598617201f98758874
ljw <jiawei.li515@gmail.com>
2025-03-17 18:05:15 +0100

    show price of product

---
+++ WebApi/Data/DogProductDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.Data
+{
+    public class DogProductDbContext(DbContextOptions<DogProductDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
